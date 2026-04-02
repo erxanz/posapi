b5c28892bfb75e353799c8537f82e6b8e937545c
erxanz <172484226+erxanz@users.noreply.github.com>
2026-04-02 09:21:57 +0700

    feat: define Outlet model relationships and fillable attributes

--- app/Models/Outlet.php
+++ app/Models/Outlet.php
@@ -8,5 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Outlet extends Model
 {
     use HasFactory;
-    //
+
+    protected $fillable = [
+        'name',
+        'owner_id',
+    ];
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+
+    public function karyawans()
+    {
+        return $this->hasMany(User::class, 'outlet_id');
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
