d5839a68b8ebe6429b8d3bd7f70b68747a8cd00f
erxanz <172484226+erxanz@users.noreply.github.com>
2026-04-06 12:08:31 +0700

    8000

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -153,7 +153,7 @@ class AuthController extends Controller
         );
 
         // LINK RESET (ganti dengan URL frontend kamu)
-        $resetLink = "http://localhost:3000/reset-password?token=$token&email=" . urlencode($user->email);
+        $resetLink = "http://localhost:8000/reset-password?token=$token&email=" . urlencode($user->email);
         // Mail::raw("Klik link berikut untuk reset password:\n$resetLink", function ($message) use ($user) {
         //     $message->to($user->email)
         //             ->subject('Reset Password');
