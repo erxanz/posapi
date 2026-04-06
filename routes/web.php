2dc737a1c4bce4b7fa77ecc9c8ed950042b016c5
erxanz <172484226+erxanz@users.noreply.github.com>
2026-04-06 12:16:52 +0700

    fix: remove app view route and associated Blade template

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{any}', function () {
-    return view('app'); // atau nama blade Vue kamu
-})->where('any', '.*');
-
 Route::get('/', function () {
     return view('welcome');
 });
