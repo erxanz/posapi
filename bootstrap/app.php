b9e5f35ec0c9ecb3fa45ad2e7fc82ce782bcb1a0
erxanz <172484226+erxanz@users.noreply.github.com>
2026-04-21 10:28:23 +0700

    fix: app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,16 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // middleware ini untuk menangkap token dari URL
-        $middleware->prepend(function ($request, $next) {
-            // Cek apakah ada parameter 'token' di URL DAN Header Authorization asli kosong
-            // Tambahkan pengecekan agar hanya berjalan jika 'token' tidak kosong
-            if ($request->filled('token') && !$request->bearerToken()) {
-                $request->headers->set('Authorization', 'Bearer ' . $request->query('token'));
-            }
-
-            return $next($request);
-        });
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
