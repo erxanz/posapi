72593c4da4e2ee6b3990eeaa7c60200ad6689ef3
erxanz <172484226+erxanz@users.noreply.github.com>
2026-04-23 10:53:07 +0700

    feat: schedule fix

--- app/Http/Controllers/ShiftKaryawanController.php
+++ app/Http/Controllers/ShiftKaryawanController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Payment;
 use App\Models\ShiftKaryawan;
+use App\Models\Schedule;
 use Illuminate\Http\Request;
 
 class ShiftKaryawanController extends Controller
